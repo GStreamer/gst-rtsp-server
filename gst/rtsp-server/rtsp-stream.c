ede027662bb157384b6eb3d57f9963e2ddf0029a
Xavier Claessens <xavier.claessens@collabora.com>
2016-09-07 10:21:09 -0400

    stream: cosmetic cleanup
    https://bugzilla.gnome.org/show_bug.cgi?id=766612

--- gst/rtsp-server/rtsp-stream.c
+++ gst/rtsp-server/rtsp-stream.c
@@ -1007,7 +1007,7 @@ no_address:
 different_address:
   {
     GST_ERROR_OBJECT (stream,
-        "address %s is not the same as %s that was already" " reserved",
+        "address %s is not the same as %s that was already reserved",
         address, (*addrp)->address);
     g_mutex_unlock (&priv->lock);
     return NULL;
